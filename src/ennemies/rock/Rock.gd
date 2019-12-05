f8316e6ecbaacba7381a2ee076bebac2d86a0e33
oliv1008 <omillochau@gmail.com>
2019-12-05 19:06:57 +0100

    structure projet

---
+++ src/ennemies/rock/Rock.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+export var rotation_speed 
+export var movement_speed
+var direction = Vector2()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
